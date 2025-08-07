56b35a95b3809a63adad54e02e5501bd20033bd7
Flor <mariaflorenciaperezd@gmail.com>
2025-08-07 14:18:08 +0200

    dasda

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Color Challengue</h1>
       <p>"Think of a color, write it and see if it matches the box"</p>
+      <p>sdfsdf</p>
       <MyFormChallenge />
     </div>
   )
